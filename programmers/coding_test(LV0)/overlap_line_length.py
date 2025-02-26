927ffd2ecdf8f774bc0885eaf8c5765f53429634
Kim_JinSeong <148855333+JinSeong0115@users.noreply.github.com>
2025-02-27 04:44:16 +0900

    Delete programmers directory

--- programmers/coding_test(LV0)/overlap_line_length.py
+++
@@ -1,6 +0,0 @@
-def solution(lines):
-    result = set()
-    for i, a in enumerate(lines):
-        for b in lines[i+1:]:
-                result = result | set(set(range(a[0], a[1])) & set(range(b[0], b[1])))
-    return len(result)
--- programmers/coding_test(LV1)/sports_uniform.py
+++
@@ -1,9 +0,0 @@
-def solution(n, lost, reserve):
-    lost, reserve = set(lost) - (set(lost)&set(reserve)), set(reserve) - (set(lost)&set(reserve))
-    
-    for r in reserve:
-        if r-1 in lost:
-            lost.remove(r-1)
-        elif r+1 in lost:
-            lost.remove(r+1)
-    return n-len(lost)
